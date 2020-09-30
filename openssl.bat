59b9a9e1c83b9800109c0915232e74362d2c72b8
ponapalt <ponapalt@shillest.net>
2020-09-30 12:33:56 +0900

    change compile option

--- openssl.bat
+++ openssl.bat
@@ -2,7 +2,7 @@ call vcvars32
 
 del /f makefile
 
-perl Configure VC-WIN32 no-dynamic-engine no-dso no-hw no-deprecated no-async no-ssl3 no-comp no-weak-ssl-ciphers enable-rc5 enable-rfc3779 enable-capieng enable-chacha enable-poly1305 -static -DWINVER=0x0500 -DWIN32_IE=0x0400 -D__FUNCTION__=\\\"\\\" -Zi
+perl Configure VC-WIN32 zlib no-dynamic-engine no-dso no-hw no-deprecated no-async no-ssl3 no-weak-ssl-ciphers no-sm3 no-sm4 no-seed no-idea no-camellia no-aria no-cast enable-rc5 enable-rfc3779 enable-capieng enable-chacha enable-poly1305 enable-ec -static -DWINVER=0x0500 -DWIN32_IE=0x0400 -D__FUNCTION__=\\\"\\\" -Zi --with-zlib-lib="../ssp_src_set/zlib-new/zlib.lib" --with-zlib-include="../ssp_src_set/zlib-new/"
 
 del /f makefile.orig
 ren makefile makefile.orig
